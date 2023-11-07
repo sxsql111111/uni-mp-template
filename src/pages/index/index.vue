d495a980f85f11c5efdc614abdc791291923fe57
sx <3049816743@qq.com>
2023-11-07 10:18:34 +0800

    add dep

---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view class="index">index</view>
+  <div class="ellipsis">哪里贵了? 这么多年都是这个价格</div>
+  <div class="flex justify-center items-center w-10 h-10 bg-pink-500"></div>
+  <div class="test bg-yellow-700 w-100 h-100"></div>
+</template>
+
+<style lang="scss">
+//
+</style>
