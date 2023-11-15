abe7399dbe061f928a96375b71c0dafec5845d9b
sx <3049816743@qq.com>
2023-11-15 22:57:07 +0800

    小程序-用户模块

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -1,24 +1,23 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-
 // 定义 Store
-export const useMemberStore = defineStore('member', () => {
-  // 会员信息
-  const profile = ref<loginResult>()
-  // 保存会员信息，登录时使用
-  const setProfile = (val: loginResult) => {
-    profile.value = val
-  }
+export const useMemberStore = defineStore(
+  'member',
+  () => {
+    // 会员信息
+    const profile = ref<LoginResult>()
+    // 保存会员信息，登录时使用
+    const setProfile = (val: LoginResult) => {
+      profile.value = val
+    }
 
-  // 清理会员信息，退出时使用
-  const clearProfile = () => {
-    profile.value = undefined
-  }
+    // 清理会员信息，退出时使用
+    const clearProfile = () => {
+      profile.value = undefined
+    }
+    //记得return
+    return { profile, setProfile, clearProfile }
+  },
 
-  // 记得 return
-  return {
-    profile,
-    setProfile,
-    clearProfile
+  {
+    unistorage: true //开启后对state的数据写都将持久化
   }
-})
+)
